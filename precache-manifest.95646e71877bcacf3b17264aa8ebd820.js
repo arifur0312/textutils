4cc93f8e726f0566b91369aaef7c1fd68ad0394e
arifur0312 <arifurrahaman0312@gmail.com>
2024-10-20 00:48:45 +0530

    Updates

---
+++ precache-manifest.95646e71877bcacf3b17264aa8ebd820.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb541d702468445d06f7752f7eb1cea",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "b62136ab133c21137e34",
+    "url": "/textutils/static/css/main.536656c8.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/textutils/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/textutils/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "b62136ab133c21137e34",
+    "url": "/textutils/static/js/main.de50e267.chunk.js"
+  },
+  {
+    "revision": "0d0a6f9eaa16f2530d12",
+    "url": "/textutils/static/js/runtime~main.5841df27.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/textutils/static/media/logo.06e73328.svg"
+  }
+]);
